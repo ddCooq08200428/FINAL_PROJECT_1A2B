2eab9b65c6aec4b0d4ee2734da2a0b2c64d31bce
ddCooq08200428 <ddrwcq0828@gmail.com>
2023-01-02 21:10:05 +0800

    first

--- FINAL_PROJECT/source/main.cpp
+++ FINAL_PROJECT/source/main.cpp
@@ -5,85 +5,31 @@
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <string.h>
+#include <time.h>
 
 using namespace std;
-/*
-class GuessNumber {
+int* aas;
+int* bbs;
+
+class GuessMyNumber {
 public:
-	GuessNumber() {
+	void GuessNumber() {
 		rng = mt19937(random_device{}());
 		for (int i = 0; i < 10000; i++) {
 			stringstream os;
 			os << setfill('0') << setw(4) << i;
 			solutions.push_back(os.str());
 		}
-		solutions.erase(remove_if(solutions.begin(), solutions.end(),
-			[](string& s) {
+		solutions.erase(remove_if(solutions.begin(), solutions.end()
+			,[](string& s) {
 				for (int j = 0; j < 4; ++j)
 					for (int k = 0; k < j; ++k)
 						if (s[j] == s[k]) return true;
 				return false;
 			}), solutions.end());
 	}
-	void trim(int A, int B) {
-		string cur = my_guess;
-		solutions.erase(remove_if(solutions.begin(), solutions.end(),
-			[=](string& str) {
-				int AA = A, BB = B;
-				for (int j = 0; j < 4; ++j) {
-					if (str[j] == cur[j]) --AA;
-					for (int k = 0; k < 4; ++k)
-						if (j != k && str[j] == cur[k]) --BB;
-				}
-				if (AA == 0 && BB == 0) return false;
-				else return true;
-			}), solutions.end());
-	}
-	string guess() {
-		uniform_int_distribution<>dis(0, size() - 1);
-		my_guess = solutions[dis(rng)];
-		return my_guess;
-	}
-	size_t size() {
-		return solutions.size();
-	}
-	void show() {
-		for (auto s : solutions)
-			cout << s << endl;
-	}
-private:
-	mt19937 rng;
-	string my_guess;
-	vector<string> solutions;
-};
-int main()
-{
 
-	GuessNumber gn;
-
-	while (gn.size() > 1) {
-		cout << gn.guess() << endl;
-		cout << "? A ? B" << endl;
-		int A, B;
-		cin >> A >> B;
-
-		gn.trim(A, B);
-		if (gn.size() < 10) {
-			cout << "------" << endl;
-			gn.show();
-			cout << "------" << endl;
-		}
-
-		if (gn.size() == 1)
-			cout << "I got it!" << endl;
-		if (gn.size() == 0)
-			cout << "No way!" << endl;
-	}
-}
-*/
-
-class GuessMyNumber {
-public:
 	GuessMyNumber() {
 		for (int i = 0; i < 10000; i++) {
 			stringstream os;
@@ -98,6 +44,22 @@ public:
 				return false;
 			}), solutions.end());
 	}
+
+	void trim(int A, int B) {
+		string cur = my_guess;
+		solutions.erase(remove_if(solutions.begin(), solutions.end(),
+			[=](string& str) {
+				int AA = A, BB = B;
+				for (int j = 0; j < 4; ++j) {
+					if (str[j] == cur[j]) --AA;
+					for (int k = 0; k < 4; ++k)
+						if (j != k && str[j] == cur[k]) --BB;
+				}
+				if (AA == 0 && BB == 0) return false;
+				else return true;
+			}), solutions.end());
+	}
+
 	vector<string> trim(int A, int B, string cur) {
 		vector<string> sols{ solutions };
 		sols.erase(remove_if(sols.begin(), sols.end(),
@@ -113,31 +75,78 @@ public:
 			}), sols.end());;
 		return sols;
 	}
+
+	void testsee(string testyyy)
+	{
+		srand(time(NULL));
+		int bbb[4] = {0};
+		int aaa[4] = { 0 };
+		int wa = 0;
+		int wb = 0;
+		string pp = testyyy;
+		char p[8];
+		int i, j;
+		for (int k = 0; k < 4; k++) {
+			bbb[k] = rand() % 8999 + 1000;
+		}
+		for (i = 0; i < pp.length(); i++)
+			p[i] = pp[i];
+		for (i = 0; i < 4; i++){
+			aaa[i] = (int)(p[i] - '0');
+			if (aaa[i] == bbb[i]){
+				wa++;
+				bbb[i] = 99;
+			}
+		}
+		for (i = 0; i < 4; i++){
+			for (j = 0; j < 4; j++){
+				if (aaa[i] == bbb[j])wb++;
+			}
+		}
+		printf("%dA%dB\n", wa, wb);
+		*aas = wa;
+		*bbs = wb;
+	}
+
+	string guess() {
+		uniform_int_distribution<>dis(0, size() - 1);
+		my_guess = solutions[dis(rng)];
+		return my_guess;
+	}
+
 	void set_solutions(vector<string>& sols){
 		solutions = sols;
 	}
+
 	size_t size() {
 		return solutions.size();
 	}
+
 	void show() {
 		for (auto s : solutions)
 			cout << s << endl;
 	}
+
 private:
+	mt19937 rng;
+	string my_guess;
 	vector<string> solutions;
 };
 
 int main()
 {
 	GuessMyNumber gn;
+	string testiii;
 	int maxguess = 8;
 	int choice;
 
-	cout << "請選擇要甚麼功能(1是我們猜，2是電腦猜，3是結束程式)" << endl;
+
+
+	cout << "請選擇要甚麼功能(1是我們猜，2是電腦猜，3是看電腦互猜，4是結束程式)" << endl;
 	cin >> choice;
-	while (choice != 3)
+	while (choice != 4)
 	{
-		if (choice == 1)
+		if (choice == 1)//自己猜數字
 		{
 			while (gn.size() > 0) {
 				cout << "Your guess: ";
@@ -169,13 +178,54 @@ int main()
 					cout << "-----" << endl;
 				}
 			}
-			cout << "請選擇要甚麼功能(1是我們猜，2是電腦猜，3是結束程式)" << endl;
+			cout << "請選擇要甚麼功能(1是我們猜，2是電腦猜，3是看電腦互猜，4是結束程式)" << endl;
 			cin >> choice;
 		}
-		else if (choice == 2)
+		else if (choice == 2)//電腦猜的程式
 		{
-			//電腦猜的程式
-			cout << "請選擇要甚麼功能(1是我們猜，2是電腦猜，3是結束程式)" << endl;
+			while (gn.size() > 0) {
+				cout << gn.guess() << endl;
+				cout << "? A ? B" << endl;
+				int A, B;
+				cin >> A >> B;
+
+				gn.trim(A, B);
+	
+				if (gn.size()  == 1) {
+					cout << "I got it!" << endl;
+					break;
+				}
+				if (gn.size() == 0) {
+					cout << "No way!" << endl;
+					break;
+				}	
+			}
+			cout << "請選擇要甚麼功能(1是我們猜，2是電腦猜，3是看電腦互猜，4是結束程式)" << endl;
+			cin >> choice;
+		}
+		else if (choice == 3)
+		{
+			while (gn.size() > 1) {
+				int A, B;
+				aas = &A;
+				bbs = &B;
+
+				testiii = gn.guess();
+				cout << testiii << endl;
+				gn.testsee(testiii);
+
+				gn.trim(A, B);
+	
+				if (A == 4) {
+					cout << "I got it!" << endl;
+					break;
+				}
+				if (gn.size() == 0) {
+					cout << "No way!" << endl;
+					break;
+				}
+			}
+			cout << "請選擇要甚麼功能(1是我們猜，2是電腦猜，3是看電腦互猜，4是結束程式)" << endl;
 			cin >> choice;
 		}
 		else
